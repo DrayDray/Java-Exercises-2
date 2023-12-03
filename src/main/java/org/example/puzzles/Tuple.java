1beaf997338584164343dc1e499ce213c76034bd
Sharon Dray <sharondray11@gmail.com>
2023-12-03 11:03:36 +0200

    Add Custom Tuple class

---
+++ src/main/java/org/example/puzzles/Tuple.java
@@ -0,0 +1,43 @@
+package main.java.org.example.puzzles;
+
+public class Tuple {
+
+    private Integer a;
+    private Integer b;
+
+    public Tuple(Integer a, Integer b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public Integer getA() {
+        return a;
+    }
+
+    public void setA(Integer a) {
+        this.a = a;
+    }
+
+    public Integer getB() {
+        return b;
+    }
+
+    public void setB(Integer b) {
+        this.b = b;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Tuple)){
+            return false;
+        }
+        Tuple objAsTuple = (Tuple) obj;
+        if (objAsTuple.getA() != this.getA()){
+            return false;
+        }
+        if(objAsTuple.getB() != this.getB()){
+            return false;
+        }
+        return true;
+    }
+}
