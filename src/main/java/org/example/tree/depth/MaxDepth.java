0f2d709afcf71562ec06e8c314cea51a606c56ab
Sharon Dray <sharondray11@gmail.com>
2023-12-24 13:21:30 +0200

    Adding Breadth First Search, fixing maven dependencies issues

--- src/main/java/org/example/tree/depth/MaxDepth.java
+++ src/main/java/org/example/tree/depth/MaxDepth.java
@@ -1,8 +1,22 @@
 package main.java.org.example.tree.depth;
 
+import org.apache.commons.collections.CollectionUtils;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+
 public class MaxDepth {
 
     public static void main(String[] args) {
+        BinaryTreeNode tree1 = createBinaryTree();
+        System.out.println("Levels:" + traverse(tree1));
+
+        BinaryTreeNode tree2 = createBinaryTree();
+        System.out.println("Levels:" + traverse2(tree1));
+    }
+
+    private static BinaryTreeNode createBinaryTree() {
         BinaryTreeNode node1 = new BinaryTreeNode(1);
         BinaryTreeNode node2 = new BinaryTreeNode(2);
         BinaryTreeNode node3 = new BinaryTreeNode(3);
@@ -12,12 +26,33 @@ public class MaxDepth {
         node2.left = node3;
         node3.left = node4;
         node4.left = node5;
+        return node1;
+    }
 
-        System.out.println(traverse(node1));
+    public static int traverse2(BinaryTreeNode root) {
+        int level = 0;
+        LinkedList<BinaryTreeNode> linkedList = new LinkedList<>(Arrays.asList(root));
+
+        while(CollectionUtils.isNotEmpty(linkedList)){
+            level++;
+            for(int i = 0; i < linkedList.size(); i++){
+                BinaryTreeNode node = linkedList.removeFirst();
+                if(node != null){
+                    if(node.left != null){
+                        linkedList.push(node.left);
+                    }
+                    if(node.right != null){
+                        linkedList.push(node.right);
+                    }
+                }
+            }
+        }
+
+        return level;
     }
 
     /**
-     * Recursive solution
+     * Recursive DFS - Depth First Search
      */
     public static int traverse(BinaryTreeNode node) {
         if (node != null) {
